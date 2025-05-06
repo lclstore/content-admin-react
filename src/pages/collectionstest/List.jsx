d4429614ba763384280549a4262c93ca18a98777
lclstore <liuchenglong@laien.io>
2025-05-06 11:40:23 +0800

    tabs切换时标题设置

--- src/pages/collectionstest/List.jsx
+++ src/pages/collectionstest/List.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Tabs } from 'antd';
 import Programs from './components/Programs';
 import Categories from './components/Categories';
 import StickyBox from 'react-sticky-box';
-const onChange = key => {
-    console.log(key);
-};
-
-
+import { HeaderContext } from '@/contexts/HeaderContext';
 
 const items = [
     {
@@ -23,10 +19,23 @@ const items = [
 ];
 
 export default function CollectionsList() {
+    const { setCustomPageTitle } = useContext(HeaderContext);
+    const defaultTabKey = '1';
+    // 页面加载时设置默认标题
+    useEffect(() => {
+        const tabBarName = items.find(item => item.key === defaultTabKey)?.label;
+        setCustomPageTitle(`${tabBarName} List`);
+    }, [setCustomPageTitle]);
+
+    const onChange = (key) => {
+        const tabBarName = items.find(item => item.key == key)?.label;
+        setCustomPageTitle(`${tabBarName} List`);
+    };
     const renderTabBar = (props, DefaultTabBar) => (
         <StickyBox offsetTop={0} style={{ zIndex: 1 }}>
             <DefaultTabBar {...props} />
         </StickyBox>
     );
-    return <Tabs defaultActiveKey="1" renderTabBar={renderTabBar} items={items} />;
+
+    return <Tabs defaultActiveKey={defaultTabKey} onChange={onChange} renderTabBar={renderTabBar} items={items} />;
 }
